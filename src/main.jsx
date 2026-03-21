ec8baadab99a53acd2ec87e6aa6db003da23f286
fertorm <luisfernando76@gmail.com>
2026-03-21 12:04:14 -0400

    Initial commit: vite react family tree app

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FamilyTree from './family-tree.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FamilyTree />
+  </React.StrictMode>,
+)
